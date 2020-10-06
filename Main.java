a2a75e7266be6be4056b8ca7835423ec838ce119
JON <jonn.thinkrtc@gmail.com>
2020-10-06 10:57:32 -0400

    add counter class

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
     
+    public static void main(String[] args) {
+       
+       counter1 = new Counter("counter1");
+       counter1.increment();
+       counter1.getCurrentValue();
+       counter1.toString();
+    }
 }
