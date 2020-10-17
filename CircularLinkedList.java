252eaf73c7989e5fa2fa082b498376448adf8c0c
JONATHAN <jonn.thinkrtc@gmail.com>
2020-10-17 10:54:07 -0400

    circularLinkedLIst

---
+++ CircularLinkedList.java
@@ -0,0 +1,46 @@
+public class CircularLinkedList{
+    Node first; 
+    Node last;
+
+    public boolean isEmpty(){
+        return (first == null);
+    }
+
+    public void insertFirst(int aNode){
+        Node newNode = new Node()
+        newNode.data = aNode;
+
+        if(isEmpty()){
+            first = newNode
+        }
+
+        newNode.next = first;
+        first = newNode;
+
+    }
+
+    public void insertLast(int aNode) {
+        Node newNode = new Node();
+        newNode.data = aNode;
+
+        if(isEmpty()){
+            first = newNode;
+        }else{
+            last.next = newNode;
+            last = newNode;
+        }
+    }
+
+    public int deleteFirst(){
+        int temp = first;
+
+        if(first.next == null){
+            last = null;
+        }
+
+        first = first.next
+        return temp
+
+    }
+
+}
