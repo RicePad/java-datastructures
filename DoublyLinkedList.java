7851bbb0ffdcc18bf08be3f81e04232145d63df0
JONATHAN <jonn.thinkrtc@gmail.com>
2020-10-21 11:30:33 -0400

    insertFirst

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -3,7 +3,31 @@ public class DoublyLinkedList{
     Node last;
 
     public DoublyLinkedList(){
+        this.first = null;
+        this.last = null;
+    
+    }
+
+
+    public boolean isEmpty(){
+        return (first == null);
+    }
+
+    public void insertFirst(int data){
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if(isEmpty()){
+            last = newNode;
+        }else {
+            first.previous = newNode;
+        }
+        
+        newNode.next = first;
+        first = newNode;
 
     }
+
+  
 
 }
