c84620597017a9f3f7cd6f2bda45b3d917537bb3
JONATHAN <jonn.thinkrtc@gmail.com>
2020-11-17 13:19:51 -0500

    recursion

--- HashTable.java
+++ HashTable.java
@@ -1,4 +1,40 @@
 //create a hash data structure
 public class HashTable{
+    String[] hashArray;
+    int arraySize;
+    int size = 0; //counter for number of elements in the hash table
+
+
+
+    public HashTable(int nOfSlots){
+        hashArray = new String[nOfSlots];
+        arraySize = nOfSlots;
+        
+    }
+
+
+    private boolean isPrime(int num){
+
+    }
+
+    private int getNextPrime(int minNumber){
+
+    }
+
+    private int hashFunc1(String word){
+
+    }
+
+    private int hashFunc2(String word){
+
+    }
+
+    public void insert(){
+
+    }
+
+    public String find(String word) {
+        
+    }
 
 }
